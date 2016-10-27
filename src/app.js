0f6b9c19be6875ad783e77ccd15f88bb6d3ef358
Maxislav <lmaxim@mail.ru>
2016-10-28 00:28:21 +0300

    start

--- src/app.js
+++ src/app.js
@@ -1,4 +1,8 @@
 "use strict";
+import "./app.styl";
+//require("./src/app.less")
 let a = 5;
 
-;
+export default class Menu{
+
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,10 @@
 /**
  * Created by maxim on 10/27/16.
  */
-
+//var LessPluginCleanCSS = require('less-plugin-clean-css');
+const webpack = require('webpack')
 module.exports = {
-    entry: './src/app.js',
+    entry: './src/app',
     output: {
         filename: 'build.js',
         path: './dist'
@@ -12,11 +13,22 @@ module.exports = {
     module: {
         loaders: [
             {
+                test: /\.styl/,
+                exclude: /(node_modules|bower_components)/,
+                loader: "style-loader!css-loader!stylus-loader"
+            },
+            {
                 test: /\.js$/,
                 loader: "babel?presets[]=es2015",
                 exclude: /(node_modules|bower_components)/
-            }
+            },
+
+        ]
+    },
+   /* lessLoader: {
+        lessPlugins: [
+            new LessPluginCleanCSS({advanced: true})
         ]
     }
-
+*/
 };
